158736a98ae2beef5a4205cf983e117946f4ee61
r.speransky <>
2018-06-26 11:26:11 +0300

    6 lesson

--- app/src/main/java/com/test/speransky/roman/android_twitter/MainActivity.kt
+++ app/src/main/java/com/test/speransky/roman/android_twitter/MainActivity.kt
@@ -2,11 +2,21 @@ package com.test.speransky.roman.android_twitter
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
+    lateinit var userImageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        userImageView = findViewById(R.id.user_image_view)
+
+        Picasso
+                .with(this)
+                .load("http://i.imgur.com/DvpvklR.png")
+                .into(userImageView)
     }
 }
